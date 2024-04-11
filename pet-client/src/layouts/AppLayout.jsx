8057b264765fbc326a5de308c063c0b213f7e4a8
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2024-04-11 10:44:43 -0500

    App Layout

--- pet-client/src/layouts/AppLayout.jsx
+++ pet-client/src/layouts/AppLayout.jsx
@@ -2,10 +2,11 @@ import { Outlet } from "react-router-dom"
 
 export default function AppLayout() {
   return (
-    <div className="flex flex-col min-h-screen w-full">
+    <div className="flex-1 w-full flex flex-col min-h-screen">
        <main className="flex-grow">
           <Outlet/>
         </main>
     </div>
   )
 }
+{/* <main className="flex-1 w-full flex flex-col items-center min-h-screen"></main> */}
